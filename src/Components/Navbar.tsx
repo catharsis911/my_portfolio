a59dc89f981f545aaa13c8219434fa1a0e4d4c03
catharsis911 <murenkoforgugl@gmail.com>
2023-12-24 22:58:27 +0300

    added style to navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -6,10 +6,10 @@ function Navbar() {
     <div className='header flex justify-center items-center bg-greyIsh h-44'>
       <div className='container w-[50%]'>
         <nav className='menu flex gap-8 justify-end'>
-         <Link to="/"><li>About</li></Link>
-         <Link to="/skills"><li>Skills</li></Link>
-         <Link to="/projects"><li>Projects</li></Link>
-         <Link to="/contacts"><li>Contacts</li></Link>
+         <Link to="/"><li className='menuList hover:text-violetColor'>About</li></Link>
+         <Link to="/skills"><li className='menuList hover:text-violetColor'>Skills</li></Link>
+         <Link to="/projects"><li className='menuList hover:text-violetColor'>Projects</li></Link>
+         <Link to="/contacts"><li className='menuList hover:text-violetColor'>Contacts</li></Link>
         </nav>
       </div>
       
