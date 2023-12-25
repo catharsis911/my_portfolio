ca9b1f1ee151cd4192cbd4cd3d2e31ce9929d1de
catharsis911 <murenkoforgugl@gmail.com>
2023-12-25 21:41:04 +0300

    fixed about section

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -12,11 +12,13 @@ function About() {
          <img src={'img.my.jpg'} className='w-[15rem] h-[15rem] rounded-full'/>
         
       </section>
-      <section className='mainInfo bg-greyIsh'>
-        <div className='max-w-[930px] mx-auto'>
+      <section className='mainInfo bg-greyIsh block'>
+        <div className='max-w-[930px] mx-auto py-[2rem]'>
           <h2 className='text-[30px] leading-12 font-bold'>About</h2>
-          <p className='text-[22px] leading-12 font-normal'>curious - hardworking - purposeful</p>
-          <p className='text-[18px] leading-7 font-normal'></p>
+          <p className='text-[22px] leading-12 font-normal my-[1rem]'>curious - hardworking - purposeful</p>
+          <p className='text-[18px] leading-7 font-normal my-[1rem]'>I have completed a large and detailed course on web development, which includes sections such as basic layout, JavaScript, and different frameworks. I am currently studying TypeScript.</p>
+          <p className='text-[18px] leading-7 font-normal my-[1rem]'>Since high school, I have been madly in love with information technology and dreamed of becoming a developer. I can't wait to apply my experience and contribute to the success of the team that likes my portfolio!</p>
+          <p className='text-[18px] leading-7 font-normal my-[1rem]'>My projects completed during my studies can be watched <a href='#projects' className='text-violetColor'> here.</a></p>
         </div>
         
 
