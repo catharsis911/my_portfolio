c72f0525204be7d264293a3944265240491f9da6
catharsis911 <murenkoforgugl@gmail.com>
2023-12-25 17:17:54 +0300

    added main section

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+
 function About() {
   return (
-    <div></div>
+    <div id='about' className=' flex justify-center'>
+      <section className='max-w-[930px] container flex flex-wrap-reverse justify-between items-center py-20'>
+        
+         <h2 className='caption text-[2.6rem] max-w-[26rem] leading-12 font-bold'>Hi, I am Olya,  
+          <span className='text-violetColor'> Junior</span> Frontend Developer
+         </h2>
+         <img src={'img.my.jpg'} className='w-[15rem] h-[15rem] rounded-full'/>
+        
+      </section>
+      <section></section>
+    </div>
   )
 }
 
