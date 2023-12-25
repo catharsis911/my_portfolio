40c04870c60f16dcfe00d6077d499c8a513febd4
catharsis911 <murenkoforgugl@gmail.com>
2023-12-25 20:19:49 +0300

    fixed display

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function About() {
   return (
-    <div id='about' className=' flex justify-center'>
-      <section className='max-w-[930px] container flex flex-wrap-reverse justify-between items-center py-20'>
+    <div id='about' className='block'>
+      <section className='max-w-[930px] container flex flex-wrap-reverse justify-between items-center py-20 mx-auto'>
         
          <h2 className='caption text-[2.6rem] max-w-[26rem] leading-12 font-bold'>Hi, I am Olya,  
           <span className='text-violetColor'> Junior</span> Frontend Developer
@@ -12,7 +12,11 @@ function About() {
          <img src={'img.my.jpg'} className='w-[15rem] h-[15rem] rounded-full'/>
         
       </section>
-      <section></section>
+      <section className='mainInfo bg-greyIsh'>
+        <h2>About</h2>
+        <p></p>
+
+      </section>
     </div>
   )
 }
