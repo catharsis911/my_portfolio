048f72e10986132a951ca18ce62f12ea1dde5088
catharsis911 <murenkoforgugl@gmail.com>
2023-12-25 21:27:34 +0300

    added about section

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -6,15 +6,19 @@ function About() {
     <div id='about' className='block'>
       <section className='max-w-[930px] container flex flex-wrap-reverse justify-between items-center py-20 mx-auto'>
         
-         <h2 className='caption text-[2.6rem] max-w-[26rem] leading-12 font-bold'>Hi, I am Olya,  
+         <h2 className='caption text-[2.6rem] max-w-[26rem] leading-12 font-bold'>Hi! I am Olya,  
           <span className='text-violetColor'> Junior</span> Frontend Developer
          </h2>
          <img src={'img.my.jpg'} className='w-[15rem] h-[15rem] rounded-full'/>
         
       </section>
       <section className='mainInfo bg-greyIsh'>
-        <h2>About</h2>
-        <p></p>
+        <div className='max-w-[930px] mx-auto'>
+          <h2 className='text-[30px] leading-12 font-bold'>About</h2>
+          <p className='text-[22px] leading-12 font-normal'>curious - hardworking - purposeful</p>
+          <p className='text-[18px] leading-7 font-normal'></p>
+        </div>
+        
 
       </section>
     </div>
