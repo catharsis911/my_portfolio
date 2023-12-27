ec531d2e36e1a24360d638e3ddbdfb6090444eec
catharsis911 <murenkoforgugl@gmail.com>
2023-12-27 18:35:13 +0300

    created footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function 
+Footer() {
+  return (
+    <div className='footer'>
+      <div className='container max-w-[930px] block'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
