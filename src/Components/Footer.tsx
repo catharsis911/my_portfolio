ed4567a5c08f1d11408aad978e241723bd5d6be7
catharsis911 <murenkoforgugl@gmail.com>
2023-12-30 20:36:42 +0300

    fixed footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,16 +6,16 @@ import { RiInstagramLine } from "react-icons/ri"
 function 
 Footer() {
   return (
-    <div className='footer'>
-      <div className='container max-w-[930px] flex items-center justify-center'>
+    <div className='footer block'>
+      <div className='container max-w-[430px] flex mx-auto my-10 items-center justify-around'>
         <div>
-         <a href=''><MdOutlineEmail className="text-[50px]"/></a>
+         <a href=''><MdOutlineEmail className="text-[50px] hover:text-violetColor" /></a>
         </div>
         <div>
-        <a href=''><PiTelegramLogo className="text-[50px]"/></a>
+        <a href=''><PiTelegramLogo className="text-[50px] hover:text-violetColor"/></a>
         </div>
         <div>
-         <a href=''><RiInstagramLine className="text-[50px]"/></a>
+         <a href=''><RiInstagramLine className="text-[50px] hover:text-violetColor"/></a>
         </div>
       </div>
     </div>
