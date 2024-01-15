9533843a113af6dc8b06c1ba7f4e21881358f644
catharsis911 <murenkoforgugl@gmail.com>
2024-01-15 18:47:23 +0300

    fixed skills

--- src/Pages/Skills.tsx
+++ src/Pages/Skills.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import Footer from '../Components/Footer'
 import { IoLogoJavascript } from "react-icons/io"
+import { TbFileTypeCss } from "react-icons/tb"
+import { IoLogoHtml5 } from "react-icons/io"
+import { FaReact } from "react-icons/fa6"
+import { SiRedux } from "react-icons/si"
+import { SiVite } from "react-icons/si"
+import { BsFillBootstrapFill } from "react-icons/bs"
+import { SiTailwindcss } from "react-icons/si"
+import { FaGithub } from "react-icons/fa"
+import { SiNpm } from "react-icons/si"
 
 function Skills() {
   return (
@@ -16,56 +25,56 @@ function Skills() {
         </div>
         <div id='css' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <TbFileTypeCss className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>CSS</h2>
           </div>
         </div>
         <div id='html' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <IoLogoHtml5 className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>HTML5</h2>
           </div>
         </div>
         <div id='react' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <FaReact className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>React</h2>
           </div>
         </div>
         <div id='redux' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <SiRedux className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>Redux</h2>
           </div>
         </div>
         <div id='vite' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <SiVite className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>Vite</h2>
           </div>
         </div>
         <div id='bootstrap' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <BsFillBootstrapFill className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>BootsTrap</h2>
           </div>
         </div>
         <div id='tailwind' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <SiTailwindcss className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>TailWind</h2>
           </div>
         </div>
         <div id='github' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <FaGithub className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>GuitHub</h2>
           </div>
         </div>
         <div id='npm' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
-            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
-            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+            <SiNpm className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>npm</h2>
           </div>
         </div>
       </section>
