135c5cf2d898efe88c73e5929c74185314022446
catharsis911 <murenkoforgugl@gmail.com>
2024-01-15 21:00:01 +0300

    added projects section

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Footer from '../Components/Footer'
 
 function Projects() {
   return (
-    <div id='projects'></div>
+    <div id='projects'>
+      <div className='block max-w-[930px] mx-auto'>
+       <div></div>
+      </div>
+     <Footer/>
+    </div>
   )
 }
 
