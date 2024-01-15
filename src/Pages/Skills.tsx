4277e552fc9827f85e48c671fe8482dc6c03967b
catharsis911 <murenkoforgugl@gmail.com>
2024-01-15 16:24:08 +0300

    added sections

--- src/Pages/Skills.tsx
+++ src/Pages/Skills.tsx
@@ -3,7 +3,33 @@ import Footer from '../Components/Footer'
 
 function Skills() {
   return (
-    <div>
+    <div id='skills' className='block'>
+      <section className='flex flex-wrap max-w-[930px]'>
+        <div id='js'>
+
+        </div>
+        <div id='css'>
+
+        </div>
+        <div id='html'>
+
+        </div>
+        <div id='react'>
+
+        </div>
+        <div id='redux'>
+
+        </div>
+        <div id='vite'>
+
+        </div>
+        <div id='bootstrap'>
+
+        </div>
+        <div id='tailwind'>
+
+        </div>
+      </section>
      <Footer/>
     </div>
   )
