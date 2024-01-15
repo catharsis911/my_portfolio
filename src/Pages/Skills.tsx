ce81ed471ea30aa5dbf07b042b841d0357c8ebd5
catharsis911 <murenkoforgugl@gmail.com>
2024-01-15 18:24:13 +0300

    added skills cards

--- src/Pages/Skills.tsx
+++ src/Pages/Skills.tsx
@@ -1,33 +1,72 @@
 import React from 'react'
 import Footer from '../Components/Footer'
+import { IoLogoJavascript } from "react-icons/io"
 
 function Skills() {
   return (
     <div id='skills' className='block'>
-      <section className='flex flex-wrap max-w-[930px]'>
-        <div id='js'>
-
+    
+      <section className='flex flex-wrap max-w-[730px] justify-between items-center py-20 px-10 mx-auto gap-10'>
+        <div id='js' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
+        
         </div>
-        <div id='css'>
-
+        <div id='css' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
         </div>
-        <div id='html'>
-
+        <div id='html' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
         </div>
-        <div id='react'>
-
+        <div id='react' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
         </div>
-        <div id='redux'>
-
+        <div id='redux' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
         </div>
-        <div id='vite'>
-
+        <div id='vite' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
         </div>
-        <div id='bootstrap'>
-
+        <div id='bootstrap' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
         </div>
-        <div id='tailwind'>
-
+        <div id='tailwind' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
+        </div>
+        <div id='github' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
+        </div>
+        <div id='npm' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
+          </div>
         </div>
       </section>
      <Footer/>
