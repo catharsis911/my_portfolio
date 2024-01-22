068ac2ab9b6fc68a52cf43d77084de506cfbf231
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-22 20:02:07 +0300

    fixed descriptions

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -6,7 +6,7 @@ function Projects() {
     <div id='projects'>
       <div className='block max-w-[930px] mx-auto'>
        <div className='bg-greyIsh flex justify-start p-8  my-14'>
-         <a href='https://catharsis911.github.io/first_portfolio/' className='hover:cursor-pointer w-[32%]'><img className='' src={'firstportfolio.PNG'}/></a>
+         <a href='https://catharsis911.github.io/first_portfolio/'  target='_blanc' className='hover:cursor-pointer w-[32%]'><img className='' src={'firstportfolio.PNG'}/></a>
          <div className='textSection block ml-10 w-[52%]'>
           <h1 className='text-[30px] font-bold'>My first portfolio</h1>
           <p className='text-[18px] leading-7 font-normal'>A portfolio of my projects that I made during the course of the <a href='https://www.freecodecamp.org/learn' className='font-bold hover:text-violetColor'>freeCodeCamp.</a></p>
@@ -18,7 +18,7 @@ function Projects() {
          <a href='' className='hover:cursor-pointer w-[32%]'><img className='' src={'mindMatch.jpg'}/></a>
          <div className='textSection block ml-10 w-[52%]'>
           <h1 className='text-[30px] font-bold'>Job searching website</h1>
-          <p className='text-[18px] leading-7 font-normal'>A portfolio of my projects that I made during the course of the <a href='https://www.freecodecamp.org/learn' className='font-bold hover:text-violetColor'>freeCodeCamp.</a></p>
+          <p className='text-[18px] leading-7 font-normal'>This is my first app written in <b>React</b> using technologies such as <b>components</b>, <b>Router</b>, <b>TailwindCss</b>.</p>
          </div>
         
         </div>
@@ -27,7 +27,7 @@ function Projects() {
          <a href='' className='hover:cursor-pointer w-[32%]'><img className='' src={'catalog.jpg'}/></a>
          <div className='textSection block ml-10 w-[52%]'>
           <h1 className='text-[30px] font-bold'>Vinyl catalog</h1>
-          <p className='text-[18px] leading-7 font-normal'>A portfolio of my projects that I made during the course of the <a href='https://www.freecodecamp.org/learn' className='font-bold hover:text-violetColor'>freeCodeCamp.</a></p>
+          <p className='text-[18px] leading-7 font-normal'>An interactive vinyl catalog written in <b>TypeScript</b> using <b>React</b>.</p>
          </div>
         
         </div>
