47cdc3f6d92f3f8fb3e4ec5dccae8b9a32a46d5d
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-22 20:01:11 +0300

    added social medias links

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -9,13 +9,13 @@ Footer() {
     <div className='footer block'>
       <div className='container max-w-[430px] flex mx-auto my-10 items-center justify-around'>
         <div>
-         <a href=""><MdOutlineEmail className="text-[50px] hover:text-violetColor transition-all duration-1000" /></a>
+         <a href="https://murenkoforgugl@gmail.com"  target='_blanc'><MdOutlineEmail className="text-[50px] hover:text-violetColor transition-all duration-1000" /></a>
         </div>
         <div>
-        <a href=''><PiTelegramLogo className="text-[50px] hover:text-violetColor transition-all duration-1000"/></a>
+        <a href='https://t.me/catisdead' target='_blanc'><PiTelegramLogo className="text-[50px] hover:text-violetColor transition-all duration-1000"/></a>
         </div>
         <div>
-         <a href=''><RiInstagramLine className="text-[50px] hover:text-violetColor transition-all duration-1000"/></a>
+         <a href='https://www.instagram.com/olyanowave?igsh=Y3p5NHhkaWg0MGds&utm_source=qr'  target='_blanc'><RiInstagramLine className="text-[50px] hover:text-violetColor transition-all duration-1000"/></a>
         </div>
       </div>
     </div>
