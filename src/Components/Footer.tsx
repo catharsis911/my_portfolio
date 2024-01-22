257fa7b43e54963c71f862742aabe2a8354269a2
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-22 18:56:33 +0300

    added transition

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -9,13 +9,13 @@ Footer() {
     <div className='footer block'>
       <div className='container max-w-[430px] flex mx-auto my-10 items-center justify-around'>
         <div>
-         <a href=""><MdOutlineEmail className="text-[50px] hover:text-violetColor" /></a>
+         <a href=""><MdOutlineEmail className="text-[50px] hover:text-violetColor transition-all duration-1000" /></a>
         </div>
         <div>
-        <a href=''><PiTelegramLogo className="text-[50px] hover:text-violetColor"/></a>
+        <a href=''><PiTelegramLogo className="text-[50px] hover:text-violetColor transition-all duration-1000"/></a>
         </div>
         <div>
-         <a href=''><RiInstagramLine className="text-[50px] hover:text-violetColor"/></a>
+         <a href=''><RiInstagramLine className="text-[50px] hover:text-violetColor transition-all duration-1000"/></a>
         </div>
       </div>
     </div>
