de4ae626af44bf556146b958bd9b2e969d4adacf
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-22 19:34:03 +0300

    added other projects

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -4,15 +4,33 @@ import Footer from '../Components/Footer'
 function Projects() {
   return (
     <div id='projects'>
-      <div className='block max-w-[930px] mx-auto p-16'>
-       <div className='bg-greyIsh flex justify-start p-8'>
-         <a href='https://catharsis911.github.io/first_portfolio/' className='hover:cursor-pointer w-[32%]'><img src={'firstportfolio.PNG'}/></a>
+      <div className='block max-w-[930px] mx-auto'>
+       <div className='bg-greyIsh flex justify-start p-8  my-14'>
+         <a href='https://catharsis911.github.io/first_portfolio/' className='hover:cursor-pointer w-[32%]'><img className='' src={'firstportfolio.PNG'}/></a>
          <div className='textSection block ml-10 w-[52%]'>
           <h1 className='text-[30px] font-bold'>My first portfolio</h1>
           <p className='text-[18px] leading-7 font-normal'>A portfolio of my projects that I made during the course of the <a href='https://www.freecodecamp.org/learn' className='font-bold hover:text-violetColor'>freeCodeCamp.</a></p>
          </div>
         
-       </div>
+        </div>
+
+        <div className='bg-greyIsh flex justify-start p-8  my-14'>
+         <a href='' className='hover:cursor-pointer w-[32%]'><img className='' src={'mindMatch.jpg'}/></a>
+         <div className='textSection block ml-10 w-[52%]'>
+          <h1 className='text-[30px] font-bold'>Job searching website</h1>
+          <p className='text-[18px] leading-7 font-normal'>A portfolio of my projects that I made during the course of the <a href='https://www.freecodecamp.org/learn' className='font-bold hover:text-violetColor'>freeCodeCamp.</a></p>
+         </div>
+        
+        </div>
+
+        <div className='bg-greyIsh flex justify-start p-8  my-14'>
+         <a href='' className='hover:cursor-pointer w-[32%]'><img className='' src={'catalog.jpg'}/></a>
+         <div className='textSection block ml-10 w-[52%]'>
+          <h1 className='text-[30px] font-bold'>Vinyl catalog</h1>
+          <p className='text-[18px] leading-7 font-normal'>A portfolio of my projects that I made during the course of the <a href='https://www.freecodecamp.org/learn' className='font-bold hover:text-violetColor'>freeCodeCamp.</a></p>
+         </div>
+        
+        </div>
       </div>
      <Footer/>
     </div>
