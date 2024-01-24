40d0023b686ff32121911c28909ec2bca252040f
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-24 17:57:07 +0300

    changed size

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
          <h2 className='caption text-[2.6rem] max-w-[26rem] leading-12 font-bold'>Hi! I am Olya,  
           <span className='text-violetColor'> Junior</span> Frontend Developer
          </h2>
-         <img src={'img.my.jpg'} className='w-[15rem] h-[15rem] rounded-full'/>
+         <img src={'img.my.jpg'} className='w-[20rem] h-[20rem] rounded-full'/>
         
       </section>
       <section className='mainInfo bg-greyIsh block'>
