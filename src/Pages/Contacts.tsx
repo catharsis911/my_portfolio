7aa89fe1989e34a6cd02fd56917e6c1d951cc3d9
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-27 16:00:32 +0300

    fixed paddings

--- src/Pages/Contacts.tsx
+++ src/Pages/Contacts.tsx
@@ -12,27 +12,32 @@ const Contacts = () => {
        <h2 className="md:text-[2.6rem] text-[2.2rem] font-bold tracking-tight text-center">CONTACT ME</h2>
        <h3 className="md:text-[1.5rem] text-[1.1rem] mb-11 text-gray-500 tracking-tight text-center">Get in toutch</h3>
       </section>
+
       <section className="block bg-greyIsh py-11 md:px-2 px-0">
+
        <div id="mail" className="flex mx-auto md:p-10 p-5 max-w-[730px] border-y-white border-y-[7px] justify-between items-center">
-        <div>
+        <div className="md:mr-11 mr-7">
         <CiMail className="md:text-[80px] text-[70px]"/>
         </div>
-        <div className="block sm:mr-40 mr-35 md:ml-10 ml-[0px]">
-         <h2 className="md:text-[1.5rem] text-[1.3rem] mt-1 font-bold">Email</h2>
+        <div className="flex justify-between grow">
+        <div className="block">
+         <h2 className="md:text-[1.5rem] text-[1.3rem] font-bold">Email</h2>
          <h3 className="md:text-[1rem] text-[0.8rem] text-gray-500">murenkoforgugl@gmail.com</h3>
         </div>
-        <div className="">
-          <button className="bg-violetColor rounded-lg md:p-3 p-2 hover:bg-violet-400 transition-all">
-            <a href="https://murenkoforgugl@gmail.com"  target='_blanc' className="text-white md:text-[1.5rem] text-[1.rem]">Get started</a>
+        <div>
+        <button className="bg-violetColor rounded-lg md:p-3 p-2 md:px-14 px-10 hover:bg-violet-400 transition-all">
+            <a href="https://murenkoforgugl@gmail.com"  target='_blanc' className="text-white md:text-[1.5rem] text-[1.rem]">Go</a>
           </button>
+          </div>
         </div>
        </div>
 
        <div id="inst" className="flex mx-auto md:p-10 p-5 max-w-[730px] border-y-white border-y-[7px] justify-between items-center mt-4">
-        <div>
+        <div className="md:mr-11 mr-7">
         <CiInstagram className="md:text-[80px] text-[70px]"/>
         </div>
-        <div className="block sm:mr-40 mr-35 md:ml-10 ml-[-60px]">
+        <div className="flex justify-between grow">
+        <div className="block">
          <h2 className="md:text-[1.5rem] text-[1.3rem] mt-1  font-bold">Instagram</h2>
          <h3 className="md:text-[1rem] text-[0.8rem]  text-gray-500">@olyanovawe</h3>
         </div>
@@ -41,13 +46,15 @@ const Contacts = () => {
             <a href="https://www.instagram.com/olyanowave?igsh=Y3p5NHhkaWg0MGds&utm_source=qr"  target='_blanc' className="text-white md:text-[1.5rem] text-[1.rem]">DM me</a>
           </button>
         </div>
+        </div>
        </div>
 
        <div id="telegram" className="flex mx-auto md:p-10 p-5 max-w-[730px] border-y-white border-y-[7px] justify-between items-center mt-4">
-        <div>
+        <div className="md:mr-11 mr-7">
         <PiTelegramLogoThin className="md:text-[80px] text-[70px]"/>
         </div>
-        <div className="block sm:mr-40 mr-35  md:ml-10 ml-[-70px]">
+        <div className="flex justify-between grow">
+        <div className="block">
          <h2 className="md:text-[1.5rem] text-[1.3rem] mt-1 font-bold">Telegram</h2>
          <h3 className="md:text-[1rem] text-[0.8rem] text-gray-500">@catisdead</h3>
         </div>
@@ -56,20 +63,23 @@ const Contacts = () => {
             <a href="https://t.me/catisdead"  target='_blanc' className="text-white md:text-[1.5rem] text-[1.rem]">Chat me</a>
           </button>
         </div>
+        </div>
        </div>
 
        <div id="github" className="flex mx-auto md:p-10 p-5 max-w-[730px] border-y-white border-y-[7px] justify-between items-center mt-4">
-        <div>
+        <div className="md:mr-11 mr-7">
         <FaGithub className="md:text-[80px] text-[70px]"/>
         </div>
-        <div className="block sm:mr-40 mr-35  md:ml-10 ml-[-80px]">
-         <h2 className="md:text-[1.5rem] text-[1.3rem] mt-1 font-bold">Github</h2>
+        <div className="flex justify-between grow">
+        <div className="block">
+         <h2 className="md:text-[1.5rem] text-[1.3rem] font-bold">Github</h2>
          <h3 className="md:text-[1rem] text-[0.8rem] text-gray-500">catharsis911</h3>
         </div>
         <div className="">
           <button className="bg-violetColor rounded-lg md:p-3 p-2 md:px-14 px-10 hover:bg-violet-400 transition-all">
             <a href="https://github.com/catharsis911"  target='_blanc' className="text-white md:text-[1.5rem] text-[1.rem]">Go</a>
           </button>
+        </div>
         </div>
        </div>
 
