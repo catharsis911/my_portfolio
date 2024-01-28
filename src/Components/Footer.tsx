516b83495008109739912627addc2873c46bc1cd
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-28 19:18:35 +0300

    fixed email link

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className='footer block px-2'>
       <div className='container max-w-[430px] flex mx-auto my-10 items-center justify-around'>
         <div>
-         <a href="https://murenkoforgugl@gmail.com"  target='_blanc'><MdOutlineEmail className="md:text-[50px] text-[40px] hover:text-violetColor transition-all duration-1000" /></a>
+         <a href="mailto:murenkoforgugl@gmail.com"  target='_blanc'><MdOutlineEmail className="md:text-[50px] text-[40px] hover:text-violetColor transition-all duration-1000" /></a>
         </div>
         <div>
         <a href='https://t.me/catisdead' target='_blanc'><PiTelegramLogo className="md:text-[50px] text-[40px] hover:text-violetColor transition-all duration-1000"/></a>
