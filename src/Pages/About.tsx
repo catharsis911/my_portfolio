cee6bc090926c0185d58b2d2297e921f2361776a
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-01-29 19:47:50 +0300

    added button

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -6,14 +6,18 @@ import Footer from '../Components/Footer'
 const About = () => {
   return (
     <div id='about' className='block'>
-      <section className='max-w-[930px] px-2 container flex flex-wrap-reverse sm:justify-between items-center py-20 mx-auto justify-center'>
-        
-         <h2 className='caption sm:text-[2.6rem] text-[1.8rem] sm:max-w-[26rem] max-w-[16rem] leading-12 font-bold'>Hi! I am Olya,  
+      <section className='max-w-[930px] px-2 container flex flex-wrap-reverse sm:justify-between items-center sm:py-20 py-16 mx-auto justify-center'>
+        <div>
+         <h2 className='caption sm:text-[2.6rem] text-[1.8rem] sm:max-w-[26rem] max-w-[16rem] leading-12 font-bold xl:my-3 my-0'>Hi! I am Olya,  
           <span className='text-violetColor'> Junior</span> Frontend Developer
          </h2>
+         <a download={'Alena.Olga.Murenko'}><button className='bg-violetColor p-3 text-white rounded-lg mt-3'>Download Resume</button></a>
+         </div>
          <img src={'img.my.jpg'} className='md:w-[20rem] w-[10rem] md:h-[20rem] h-[10rem] rounded-full'/>
         
       </section>
+      
+      
       <section className='mainInfo bg-greyIsh block px-2'>
         <div className='max-w-[930px] mx-auto py-[2rem]'>
           <h2 className='md:text-[30px] text-[25px] leading-12 font-bold'>About</h2>
