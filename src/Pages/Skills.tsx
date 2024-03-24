87540fe64d9e0799ad6418c823a1c6f6b028e9ea
catharsis911 <127673819+catharsis911@users.noreply.github.com>
2024-03-24 22:52:31 +0300

    added some skills

--- src/Pages/Skills.tsx
+++ src/Pages/Skills.tsx
@@ -10,71 +10,85 @@ import { BsFillBootstrapFill } from "react-icons/bs"
 import { SiTailwindcss } from "react-icons/si"
 import { FaGithub } from "react-icons/fa"
 import { SiTypescript } from "react-icons/si"
+import { SiWebpack } from "react-icons/si";
+import { SiReactquery } from "react-icons/si";
 
 function Skills() {
   return (
     <div id='skills' className='block px-2'>
     
-      <section className='flex flex-wrap max-w-[730px] md:justify-between justify-center items-center py-20 px-10 mx-auto gap-10'>
-        <div id='js' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+      <section className='flex flex-wrap max-w-[700px] md:justify-between justify-center items-center py-20 px-10 mx-auto gap-6'>
+        <div id='js' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <IoLogoJavascript className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>JavaScript</h2>
           </div>
         
         </div>
-        <div id='css' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='css' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <TbFileTypeCss className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>CSS</h2>
           </div>
         </div>
-        <div id='html' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='html' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <IoLogoHtml5 className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>HTML5</h2>
           </div>
         </div>
-        <div id='react' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='react' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <FaReact className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>React</h2>
           </div>
         </div>
-        <div id='redux' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='redux' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <SiRedux className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>Redux</h2>
           </div>
         </div>
-        <div id='vite' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='vite' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <SiVite className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>Vite</h2>
           </div>
         </div>
-        <div id='bootstrap' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='bootstrap' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <BsFillBootstrapFill className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>BootsTrap</h2>
           </div>
         </div>
-        <div id='tailwind' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='tailwind' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <SiTailwindcss className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>TailWind</h2>
           </div>
         </div>
-        <div id='github' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='github' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <FaGithub className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>GuitHub</h2>
           </div>
         </div>
-        <div id='npm' className='w-[300px] flex justify-center py-8 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+        <div id='npm' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
           <div className='block'>
             <SiTypescript className="text-[50px] mb-3 text-violetColor mx-auto"/>
             <h2 className='text-[20px] mx-auto font-bold'>TypeScript</h2>
+          </div>
+        </div>
+        <div id='webpack' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <SiWebpack className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>Webpack</h2>
+          </div>
+        </div>
+        <div id='query' className='w-[250px] flex justify-center py-6 border-[1px] border-[#ededed] rounded-xl shadow-md'>
+          <div className='block'>
+            <SiReactquery className="text-[50px] mb-3 text-violetColor mx-auto"/>
+            <h2 className='text-[20px] mx-auto font-bold'>React Query</h2>
           </div>
         </div>
       </section>
